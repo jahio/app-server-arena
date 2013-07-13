bd4badfcf27a0aa809e1c9fafba6aebeafc28d03
J. Austin Hughey <jaustinhughey@me.com>
2013-07-13 06:09:13 -0500

    hooks for restarting if envname == asa_thin

---
+++ deploy/after_restart.rb
@@ -0,0 +1,15 @@
+# Thin doesn't come standard on Engine Yard, so after restart, if this is
+# a thin app, shut off unicorn and turn on thin.
+if environment_name == 'asa_thin'
+  # Start thin ASAP
+  run! "/data/#{app}/shared/config/thin_control.sh restart"
+    # Will log errors if thin isn't already running but that's no biggie,
+    # it'll still launch thin. Will do a rolling restart of workers
+    # if chef has properly run.
+
+  # Stop Unicorn
+  sudo! "monit stop all -g unicorn_#{app} && sleep 1"
+
+  # Unmonitor Unicorn
+  sudo! "monit unmonitor all -g unicorn_#{app}"
+end
